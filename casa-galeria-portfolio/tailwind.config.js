345d3453bff66434456368c12f46caf170ae8be4
Jay Mansur <jgmansur2@gmail.com>
2026-03-21 12:21:48 -0600

    feat: link casa-galeria-portfolio to musicknobs repo

---
+++ casa-galeria-portfolio/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand': {
+          DEFAULT: '#0f172a',
+          light: '#334155',
+          gold: '#d4af37'
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Outfit', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
